8835d8d90af6cf15cbdb6f523bb7a67e7992a96a
Patrick Oliver <115066967+PatrickOliverB@users.noreply.github.com>
2022-11-17 11:55:05 +0100

    added postman link

--- block6-path-variable-headers/src/main/java/com/example/block6pathvariableheaders/Block6PathVariableHeadersApplication.java
+++ block6-path-variable-headers/src/main/java/com/example/block6pathvariableheaders/Block6PathVariableHeadersApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //El servidor se lanza en el puerto 8081 (se puede cambiar en application.properties)
 // por tanto los request se realizan en localhost:8081
+//https://www.getpostman.com/collections/91b2a0224e7f2748955b
 @SpringBootApplication
 public class Block6PathVariableHeadersApplication {
 
