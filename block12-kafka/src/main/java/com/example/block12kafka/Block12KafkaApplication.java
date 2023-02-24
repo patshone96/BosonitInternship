9a0350e335d7b06df257f9d900e5843902acab2c
Patrick Oliver <patrick.oliver@bosonit.com>
2023-02-24 11:59:50 +0100

    Block12 Kafka - Base Project

---
+++ block12-kafka/src/main/java/com/example/block12kafka/Block12KafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.block12kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Block12KafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Block12KafkaApplication.class, args);
+	}
+
+}
---
+++ block12-kafka/src/test/java/com/example/block12kafka/Block12KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.block12kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Block12KafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
