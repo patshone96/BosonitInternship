b831ecaa6298b684a2205c8b48d07d4d3b8fdf6d
Patrick Oliver <patrick.oliver@bosonit.com>
2023-02-24 16:07:57 +0100

    Block12 Kafka - Kafka2, now receives messages from endpoints specified on Kafka

---
+++ Block12-Kafka2/src/main/java/com/example/Block12Kafka2/Block12Kafka2Application.java
@@ -0,0 +1,13 @@
+package com.example.Block12Kafka2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Block12Kafka2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Block12Kafka2Application.class, args);
+	}
+
+}
---
+++ Block12-Kafka2/src/test/java/com/example/Block12Kafka2/Block12Kafka2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Block12Kafka2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Block12Kafka2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
