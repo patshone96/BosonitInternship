e3e17be0e1da5b699b98c0710edbfe7a3e06df53
Patrick Oliver <patrick.oliver@bosonit.com>
2023-03-03 15:22:48 +0100

    JPA_EXAM - Finished

--- examen_JPA_cascada/src/main/java/com/example/examen_JPA_cascada/CabeceraFra/service/CabeceraFraImpl.java
+++ examen_JPA_cascada/src/main/java/com/example/examen_JPA_cascada/CabeceraFra/service/CabeceraFraImpl.java
@@ -24,41 +24,59 @@ public class CabeceraFraImpl implements CabeceraFraService {
     @Autowired
     LineasFraRepository lineasFraRepository;
 
+    //Saves a CabeceraFra to the Repository
     @Override
     public CabeceraFra addCabecera(CabeceraFra cab) {
        return cabeceraFraRepository.save(cab);
 
     }
 
+    //Delete a cabecera by id. It also deletes all data associated to them
     @Override
     public String deleteCabecera(Integer id) {
 
+        //We check for the CabeceraFra on the repository
         Optional<CabeceraFra> op = cabeceraFraRepository.findById(id);
 
-        if (op.isEmpty()){
+        if (op.isEmpty()){ //If there isn't any CabeceraFra with the provided ID, an exception is thrown
             throw new NoResultException("Factura not found");
         }else{
+
+            //Recover the CabeceraFra object from the optional
+            CabeceraFra cab = op.get();
+
+            //Recover the CabeceraFra Lineas
+            List<LineasFra> lineasCab = cab.getLineasFra();
+
+            //Delete the lines one by one using the deleteById using the ID getter
+            lineasCab.stream().forEach(p -> lineasFraRepository.deleteById(p.getLinFraID()));
+
+            //Finally, we delete the CabeceraFra
             cabeceraFraRepository.deleteById(id);
         }
-
-
 
         return String.format("Factura %d eliminada", id);
     }
 
+    //Shows all the Facturas stored
     @Override
     public List<FacturaOutputDTO> getFacturas() {
 
+
         List<FacturaOutputDTO> facturas = new ArrayList<>();
 
+        //We recover the data from the cabeceraFra repo and cast every object to FacturaOutputDTO
+        //These are stored in the list facturas
         cabeceraFraRepository.findAll().stream().forEach(p -> {
             facturas.add(new FacturaOutputDTO(p));
         });
 
+        //Return the list
         return facturas;
 
     }
 
+    //Adds a line to a given Factura
     @Override
     public FacturaOutputDTO addLine(LineasFraInputDTO linea, Integer id) {
 
@@ -67,28 +85,33 @@ public class CabeceraFraImpl implements CabeceraFraService {
             throw new InputMismatchException("The id provided in the URL and the FraID have to match");
         }
 
+            //Check if there is a Cabecera with the provided ID
            Optional<CabeceraFra> op = cabeceraFraRepository.findById(linea.getIdFra());
 
-           if (op.isEmpty()){
+           if (op.isEmpty()){ //If there isn't a cabecera, we throw an exception
                throw new NoResultException("Factura not found");
                }
 
+           //If there is a Cabecera, we retrieve it from the Optional.
            CabeceraFra cab = op.get();
 
+           //Declare a line object
            LineasFra newLine = new LineasFra();
 
+
+           // Set the attributes of the new line
            newLine.setPrice(linea.getPrice());
            newLine.setQuantity(linea.getQuantity());
            newLine.setProdName(linea.getProdName());
 
+
+           // Save the line created with the .save method from the lineas repo
+          //And as this method returns an instance of the Lineas object
+          //We add the instance to the lineasFra of cabecera
            cab.getLineasFra().add(lineasFraRepository.save(newLine));
 
+           //Finally, return a FacturaDTO
            return new FacturaOutputDTO(cabeceraFraRepository.save(cab));
-
-
-
-
-
 
     }
 }
