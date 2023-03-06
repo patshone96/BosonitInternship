5de75098485fcbdab302153ec3f1ea0ffeb641f0
Patrick Oliver <patrick.oliver@bosonit.com>
2023-03-06 12:24:34 +0100

    Block13-MongoDB CRUD base structure (+Person entity)

--- Block13-MongoDB/src/main/java/com/example/Block13MongoDB/Person/model/Person.java
+++ Block13-MongoDB/src/main/java/com/example/Block13MongoDB/Person/model/Person.java
@@ -1,4 +1,37 @@
 package com.example.Block13MongoDB.Person.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
+
+    @Id
+    private long id;
+
+
+    private String usr;
+    private String password;
+    private String name;
+    private String surname;
+    private String city;
+    private String companyMail;
+    private String personalMail;
+    private Boolean active;
+    private Date createdDate;
+    private String imageUrl;
+    private Date terminationDate;
+
+
+
+
 }
