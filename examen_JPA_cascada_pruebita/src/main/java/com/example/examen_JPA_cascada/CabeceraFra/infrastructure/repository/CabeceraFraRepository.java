0fac8164af7d8ec521580b8e52840e7bbdfb19a9
Patrick Oliver <115066967+PatrickOliverB@users.noreply.github.com>
2023-03-06 11:14:10 +0100

    Delete examen_JPA_cascada_pruebita directory

--- examen_JPA_cascada_pruebita/src/main/java/com/example/examen_JPA_cascada/CabeceraFra/infrastructure/repository/CabeceraFraRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.examen_JPA_cascada.CabeceraFra.infrastructure.repository;
-
-import com.example.examen_JPA_cascada.CabeceraFra.model.CabeceraFra;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CabeceraFraRepository extends JpaRepository<CabeceraFra, Integer> {
-}
--- examen_JPA_cascada_pruebita/src/main/java/com/example/examen_JPA_cascada/LineasFra/infrastructure/repository/LineasFraRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.examen_JPA_cascada.LineasFra.infrastructure.repository;
-
-import com.example.examen_JPA_cascada.LineasFra.model.LineasFra;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LineasFraRepository extends JpaRepository<LineasFra, Integer> {
-}
